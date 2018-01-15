9b9947447baf0e604eb96f504c3c0faada075a4f
Talon <asd@asd.com>
2018-01-15 12:18:58 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {UserComponent} from "./user.component";
-import { UserDetailComponent } from './user-detail/user-detail.component';
 
 
 @NgModule({
